682809f1d5611168bb2eba9ae7cd45f5af01c357
Binil B <104673899+thegr8binil@users.noreply.github.com>
2022-06-18 00:56:08 +0530

    Twosum.java
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    You can return the answer in any order.

---
+++ Twosum.java
@@ -0,0 +1,16 @@
+ 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] nos= new int[2];
+        for(int i=0; i<=nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                nos[0]=i;
+                nos[1]=j;
+                break;
+            }
+        }
+        }
+        return nos;
+    }
+}
